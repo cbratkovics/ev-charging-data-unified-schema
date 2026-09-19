81280384b16d411126fe7638680ffa58de38cbba
Christopher Bratkovics <cbratkovics@gmail.com>
2026-09-19 18:35:21 -0400

    chore: strip prediction pipeline, keep chassis

--- dbt/macros/period_key.sql
+++
@@ -1,6 +0,0 @@
-{#- period_key = season * period_key_base + period: monotonic in time, one integer for window
-    frames and lookbacks. period is an ordinal 1..N within the season; period_key_base (var,
-    mirrored from config) is the next power of ten above N, so the key never collides. -#}
-{% macro period_key(season_expr, period_expr) -%}
-    ({{ season_expr }} * {{ var('period_key_base') }} + {{ period_expr }})
-{%- endmacro %}
